b9398958d494a6217c58b18886e8b5425e7836fb
Titty <mehran9326@gmail.com>
2024-05-06 18:50:57 +0330

    scroll bar and page transition added

--- src/app/template.jsx
+++ src/app/template.jsx
@@ -1,3 +1,41 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+import useScrollProgress from "@/hooks/useScrollProgress";
+
+const pageVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  enter: {
+    opacity: 1,
+    transition: {
+      type: "linear",
+      delay: 0.25,
+      duration: 0.5,
+    },
+  },
+};
+
 export default function Template({ children }) {
-  return <main className="flex-1">{children}</main>;
+  const scrollProgress = useScrollProgress();
+
+  return (
+    <>
+      <motion.main
+        className="flex-1"
+        variants={pageVariants}
+        initial="hidden"
+        animate="enter"
+      >
+        {children}
+      </motion.main>
+      ;
+      <span
+        className="w-1 fixed top-0 right-0 bottom-0 bg-primary transition-all duration-700 ease-in-out"
+        style={{ transform: `translateY(${scrollProgress - 100}%)` }}
+      />
+    </>
+  );
 }
